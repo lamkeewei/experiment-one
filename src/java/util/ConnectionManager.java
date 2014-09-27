667f864f91bd39838e0daa7677dd1ed85fc88120
Lam Kee Wei <keewei.lam@gmail.com>
2014-09-27 16:12:24 +0800

    Added connection pooling

--- src/java/util/ConnectionManager.java
+++ src/java/util/ConnectionManager.java
@@ -1,76 +1,92 @@
 package util;
 
 import java.sql.*;
-import java.util.Properties;
-import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 public class ConnectionManager {
-    
-  private static String dbUser = "root";
-  private static String dbPassword = "";
-  private static String dbURL = "jdbc:mysql://localhost:3306/stocks_exchange";
 
-  static {
-    try {
-      Class.forName("com.mysql.jdbc.Driver").newInstance();
-    } catch (Exception ex) {
-      String message = "Unable to find JDBC driver for MySQL.";
+    private static String dbUser = "root";
+    private static String dbPassword = "";
+    private static String dbURL = "jdbc:mysql://localhost:3306/stocks_exchange";
 
-      System.out.println(message);
-      Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, message, ex);
-      throw new RuntimeException(message, ex);
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (Exception ex) {
+            String message = "Unable to find JDBC driver for MySQL.";
+
+            System.out.println(message);
+            Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, message, ex);
+            throw new RuntimeException(message, ex);
+        }
     }
-  }
 
-  /**
-   * Gets a connection to the database
-   *
-   * @return the connection
-   * @throws SQLException if an error occurs when connecting
-   */
-  public static Connection getConnection() throws SQLException {
-    String message = "dbURL: " + dbURL
-            + "  , dbUser: " + dbUser
-            + "  , dbPassword: " + dbPassword;
+    /**
+     * Gets a connection to the database
+     *
+     * @return the connection
+     * @throws SQLException if an error occurs when connecting
+     */
+    public static Connection getConnection() throws SQLException {
+//    String message = "dbURL: " + dbURL
+//            + "  , dbUser: " + dbUser
+//            + "  , dbPassword: " + dbPassword;
+//
+//    return DriverManager.getConnection(dbURL, dbUser, dbPassword);
 
-    return DriverManager.getConnection(dbURL, dbUser, dbPassword);
+        Context initCtx;
+        Connection conn = null;
+        try {
+            initCtx = new InitialContext();
+            Context envCtx = (Context) initCtx.lookup("java:comp/env");
 
-  }
-
-  /**
-   * close the given connection, statement and resultset
-   *
-   * @param conn the connection object to be closed
-   * @param stmt the statement object to be closed
-   * @param rs the resultset object to be closed
-   */
-  public static void close(Connection conn, Statement stmt, ResultSet rs) {
-    try {
-      if (rs != null) {
-        rs.close();
-      }
-    } catch (SQLException ex) {
-      Logger.getLogger(ConnectionManager.class.getName()).log(Level.WARNING, 
-              "Unable to close ResultSet", ex);
+            // Look up our data source
+            DataSource ds = (DataSource) envCtx.lookup("jdbc/stocks_exchange");
+            // Allocate and use a connection from the pool
+            conn = ds.getConnection();
+        } catch (NamingException ex) {
+            Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return conn;
     }
-    try {
-      if (stmt != null) {
-        stmt.close();
-      }
-    } catch (SQLException ex) {
-      Logger.getLogger(ConnectionManager.class.getName()).log(Level.WARNING, 
-              "Unable to close Statement", ex);
+
+    /**
+     * close the given connection, statement and resultset
+     *
+     * @param conn the connection object to be closed
+     * @param stmt the statement object to be closed
+     * @param rs the resultset object to be closed
+     */
+    public static void close(Connection conn, Statement stmt, ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionManager.class.getName()).log(Level.WARNING,
+                    "Unable to close ResultSet", ex);
+        }
+        try {
+            if (stmt != null) {
+                stmt.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionManager.class.getName()).log(Level.WARNING,
+                    "Unable to close Statement", ex);
+        }
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionManager.class.getName()).log(Level.WARNING,
+                    "Unable to close Connection", ex);
+        }
     }
-    try {
-      if (conn != null) {
-        conn.close();
-      }
-    } catch (SQLException ex) {
-      Logger.getLogger(ConnectionManager.class.getName()).log(Level.WARNING, 
-              "Unable to close Connection", ex);
-    }
-  }  
 }
-
