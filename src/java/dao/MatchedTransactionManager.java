94e16a15e04638c070ecf95d23dbf2ca22c8fbac
Lam Kee Wei <keewei.lam@gmail.com>
2014-09-27 11:50:06 +0800

    Added retrieve matched by stock

--- src/java/dao/MatchedTransactionManager.java
+++ src/java/dao/MatchedTransactionManager.java
@@ -10,6 +10,7 @@ import aa.Bid;
 import aa.MatchedTransaction;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
@@ -47,7 +48,50 @@ public class MatchedTransactionManager {
     
     public static List<MatchedTransaction> getMatchedTransactionByStock(String stock){
         List<MatchedTransaction> matched = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
         
-        return matched;
+        try {
+            conn = ConnectionManager.getConnection();
+            String sql = "select * from matched_transaction M, bids B, asks A " +
+                            "where M.bid_id = B.id " +
+                            "and M.ask_id = A.id " + 
+                            "and M.stock=?";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, stock);
+            rs = pstmt.executeQuery();
+            
+            while (rs.next()) {
+                int bidId = rs.getInt("B.id");
+                String bidStock = rs.getString("B.stock");
+                int bidPrice = rs.getInt("B.price");
+                String bidUserId = rs.getString("B.userid");
+                Date bidDate = rs.getDate("B.date");
+                String bidStatus = rs.getString("B.status");
+                
+                Bid bid = new Bid(bidId, stock, bidPrice, bidUserId, bidDate, bidStatus);
+                
+                int askId = rs.getInt("A.id");
+                String askStock = rs.getString("A.stock");
+                int askPrice = rs.getInt("A.price");
+                String askUserId = rs.getString("A.userid");
+                Date askDate = rs.getDate("A.date");
+                String askStatus = rs.getString("A.status");
+                
+                Ask ask = new Ask(askId, askStock, askPrice, askUserId, askDate, askStatus);
+                
+                Date matchedDate = rs.getDate("M.date");
+                int matchedPrice = rs.getInt("M.price");
+                
+                MatchedTransaction mtxn = new MatchedTransaction(bid, ask, matchedDate, matchedPrice);
+                matched.add(mtxn);
+            }
+            
+            return matched;
+        } catch (SQLException ex) {
+            Logger.getLogger(MatchedTransactionManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
 }
